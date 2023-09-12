78cfa84c943629807c5e6a5df04de5854edad51b
krishnanjalivu <vukrishnanjali@gmail.com>
2023-09-12 12:35:32 +0530

    first commit

---
+++ task 1/index.js
@@ -0,0 +1,33 @@
+const select = document.querySelector("select");
+
+
+select.addEventListener("change", () => {
+    const selectedRange = select.value;
+
+    if (selectedRange === "0-10") {
+        document.getElementById("plans1").classList.add("highlight");
+    } else if (selectedRange === "10-20") {
+        document.getElementById("plans2").classList.add("highlight");
+    } else if (selectedRange === "30 and above") {
+        document.getElementById("plans3").classList.add("highlight");
+    }
+});
+
+
+
+
+document.querySelector("#showform1").addEventListener("click",function()
+{
+    document.querySelector(".popupform").classList.add("active");
+})
+document.querySelector("#showform2").addEventListener("click",function()
+{
+    document.querySelector(".popupform").classList.add("active");
+})
+document.querySelector("#showform3").addEventListener("click",function()
+{
+    document.querySelector(".popupform").classList.add("active");
+})
+document.querySelector(".popupform .wrapper .close").addEventListener("click",function(){
+    document.querySelector(".popupform").classList.remove("active");
+})
